98acf2e83f168a5739171e055bdf2267d69da648
Michel Davalos <michel.davalos@academicos.udg.mx>
2022-01-04 12:02:02 -0600

    added %userprofile% for a generic location

---
+++ inicializador/impresoras.bat
@@ -0,0 +1,6 @@
+START ""  "%userprofile%\Desktop\Network ticket_a.exe.lnk"
+timeout /t 5 /nobreak
+START "" "%userprofile%\Desktop\Network ticket_b.exe.lnk"
+timeout /t 5 /nobreak
+START "" "%userprofile%\Desktop\Network ticket_c.exe.lnk"
+timeout /t 5 /nobreak
