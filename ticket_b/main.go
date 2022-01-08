5cd697c51dad750bfc8da9a0f497f63e6b72ee87
Michel Davalos <michel.davalos@academicos.udg.mx>
2022-01-08 16:02:45 -0600

    feat: Add PTerm to show priter's name WithLetters

--- ticket_b/main.go
+++ ticket_b/main.go
@@ -3,11 +3,17 @@ package main
 import (
 	"fmt"
 	"time"
+	"github.com/pterm/pterm"
 )
 
 func main()  {
-	fmt.Println("Impresora Ticket B")
-	
+	// fmt.Println("Impresora Ticket B")
+	// Print a large text with differently colored letters.
+    pterm.DefaultBigText.WithLetters(
+        pterm.NewLettersFromStringWithStyle("I:", pterm.NewStyle(pterm.FgLightGreen)),
+        pterm.NewLettersFromStringWithStyle("Ticket B", pterm.NewStyle(pterm.FgLightYellow))).
+        Render()
+
 	go func ()  {
 		for {
 			time.Sleep(time.Second)
