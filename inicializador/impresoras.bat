58b3ce512914474b6e5e92bcddfc4b34daee50a8
Michel Davalos <michel.davalos@academicos.udg.mx>
2022-01-08 15:44:18 -0600

    feat: Add USB Serve.exe as a first command to run

--- inicializador/impresoras.bat
+++ inicializador/impresoras.bat
@@ -1,6 +1,8 @@
-START ""  "%userprofile%\Desktop\Network ticket_a.exe.lnk"
+START "" "C:\Program Files (x86)\USB Server 2\USB Server.exe"
+timeout /t 10 /nobreak
+START ""  "%userprofile%\Desktop\impresoras\arranque\Network ticket_a.exe.lnk"
 timeout /t 5 /nobreak
-START "" "%userprofile%\Desktop\Network ticket_b.exe.lnk"
+START "" "%userprofile%\Desktop\impresoras\arranque\Network ticket_b.exe.lnk"
 timeout /t 5 /nobreak
-START "" "%userprofile%\Desktop\Network ticket_c.exe.lnk"
+START "" "%userprofile%\Desktop\impresoras\arranque\Network ticket_c.exe.lnk"
 timeout /t 5 /nobreak
